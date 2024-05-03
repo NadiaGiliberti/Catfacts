2292211f4f5fbfab7722f69eadfcc3c18a3368b9
NadiaGiliberti <nadia.giliberti@stud.fhgr.ch>
2024-05-03 16:59:38 +0200

    Popup bei öffnen von Seite

---
+++ js/catfacts.js
@@ -0,0 +1,66 @@
+const searchBox = document.querySelector('#searchBox');
+const catFactsApp = document.querySelector('#catFactsApp');
+let url = 'https://cat-fact.herokuapp.com/facts/random?amount=300'; // verlinkung zu IPA Datenbank (Fakten)
+let url_2 = 'https://api.thecatapi.com/v1/images/search?limit=10'; // verlinkung zu zweiter IPA Datenbank (Fotos)
+
+init();
+
+async function init() {
+    let catFacts = await fetchData(url);
+    let catImage = await fetchImage(url_2);
+    console.log(catFacts[0].text);
+    catFacts.forEach(catFact => {
+        createItem(catFact, catImage);
+    });
+
+}
+
+async function search() {
+    let searchValue = searchBox.value;
+    let url = `https://cat-fact.herokuapp.com/facts/random?amount=300&text=${searchValue}`;
+    catFactsApp.innerHTML = '';
+    let catFacts = await fetchData(url);
+    let catImage = await fetchImage(url_2);
+    catFacts.forEach(catFact => {
+        createItem(catFact, catImage);
+    });
+
+}
+
+searchBox.addEventListener('input', search);
+
+function createItem(catFact, catImage) {
+    let item = document.createElement('div');
+    item.classList.add('catFact');
+    item.innerHTML = `
+        <p>${catFact.text}</p>
+        <img src="${catImage[0].url}" alt="Cat Image">
+        `;
+    catFactsApp.appendChild(item);
+
+}
+
+
+// Hier werden die Daten aus der API geholt:
+async function fetchData(url) {
+    try {
+        let response = await fetch(url);
+        let data = await response.json();
+        let verifiedFacts = data.filter(fact => fact.status.verified === true);
+        return verifiedFacts;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+async function fetchImage(url_2) {
+    try {
+        let response = await fetch(url_2);
+        let data = await response.json();
+        return data;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
--- js/script.js
+++ js/script.js
@@ -1,65 +1,17 @@
-const searchBox = document.querySelector('#searchBox');
 const catFactsApp = document.querySelector('#catFactsApp');
-let url = 'https://cat-fact.herokuapp.com/facts/random?amount=500'; // verlinkung zu IPA Datenbank (Fakten)
-let url_2 = 'https://api.thecatapi.com/v1/images/search?limit=10'; // verlinkung zu zweiter IPA Datenbank (Fotos)
 
-init ();
+// POP UP:
+document.addEventListener("DOMContentLoaded", function () {
+    openPopup();  // Zeige das Popup an, wenn die Seite geladen ist
 
-async function init(){
-    let catFacts = await fetchData(url);
-    let catImage = await fetchImage(url_2);
-    console.log(catFacts[0].text);
-    catFacts.forEach(catFact => {
-        createItem(catFact, catImage);
-    });
-
-}
-
-async function search(){
-let searchValue = searchBox.value;
-let url = `https://cat-fact.herokuapp.com/facts/random?amount=500&text=${searchValue}`;
-catFactsApp.innerHTML = '';
-let catFacts = await fetchData(url);
-let catImage = await fetchImage(url_2);
-catFacts.forEach(catFact => {
-    createItem(catFact, catImage);
 });
 
+function openPopup() {
+    var popup = document.getElementById("popup");
 }
 
-searchBox.addEventListener('input', search);
+function closePopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
 
-function createItem(catFact, catImage) {
-    let item = document.createElement('div');
-    item.classList.add('catFact');
-    item.innerHTML = `
-        <p>${catFact.text}</p>
-        <img src="${catImage[0].url}" alt="Cat Image">
-        `;
-    catFactsApp.appendChild(item);
-}
-
-
-// Hier werden die Daten aus der API geholt:
-async function fetchData(url) {
-    try {
-        let response = await fetch(url);
-        let data = await response.json();
-        let verifiedFacts = data.filter(fact => fact.status.verified === true);
-        return verifiedFacts;
-    }
-    catch (error) {
-        console.log(error);
-    }
-}
-
-async function fetchImage(url_2) {
-    try {
-        let response = await fetch(url_2);
-        let data = await response.json();
-        return data;
-    }
-    catch (error) {
-        console.log(error);
-    }
 }
