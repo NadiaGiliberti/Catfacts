d0e8af4d8ec16d2a0d2c33058fa414e9f2e5d633
Suenaf <suena.fischer@stud.fhgr.ch>
2024-05-06 11:24:59 +0200

    Counter update

--- js/startseite.js
+++ js/startseite.js
@@ -24,3 +24,46 @@ document.addEventListener('mousemove', function(e) {
     console.log("maus wurde bewegt:", posX, posY);
 
 })
+
+// Initialisieren der Klickanzahl im sessionStorage, falls noch nicht vorhanden
+if (!sessionStorage.getItem('clickCount')) {
+    sessionStorage.setItem('clickCount', '0');
+}
+
+// Laden der aktuellen Klickanzahl
+var clickCount = parseInt(sessionStorage.getItem('clickCount'));
+
+// Funktion zur Aktualisierung der Klickanzahl-Anzeige
+function updateClickCount() {
+    // Laden der aktuellen Klickanzahl
+    clickCount = parseInt(sessionStorage.getItem('clickCount'));
+    // Aktualisieren der Anzeige der Klickanzahl auf dem Bildschirm
+    document.getElementById("clickNumber").textContent = clickCount;
+}
+
+function handleMouseClick(event) {
+    // Überprüfen, ob das geklickte Element die ID "mouse" hat
+    if (event.target.id === "mouse") {
+        // Inkrementiere die Klickanzahl
+        clickCount++;
+        // Speichere die aktualisierte Klickanzahl im SessionStorage
+        sessionStorage.setItem("clickCount", clickCount.toString());
+        console.log("Erfolgreicher Klick! Gesamtanzahl: " + clickCount);
+
+        // Aktualisiere die Anzeige der Klickanzahl auf dem Bildschirm
+        updateClickCount();
+    }
+}
+
+// Füge den Eventlistener nur dem Element mit der ID "mouse" hinzu, nachdem das DOM vollständig geladen ist
+document.addEventListener("DOMContentLoaded", function() {
+    // Aktualisiere die Anzeige der Klickanzahl, wenn das DOM geladen ist
+    updateClickCount();
+    // Füge den Eventlistener hinzu
+    document.getElementById("mouse").addEventListener("click", handleMouseClick);
+});
+
+
+
+
+
