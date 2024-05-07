1dfb494a7b33df791121c3259c76ad44c2d6b89d
Suenaf <suena.fischer@stud.fhgr.ch>
2024-05-07 12:00:30 +0200

    Popup mit Fact und Bild

--- js/startseite.js
+++ js/startseite.js
@@ -121,4 +121,69 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+const popupFact = document.querySelector('#catFact');
 
+init('https://cat-fact.herokuapp.com/facts/random?amount=300', 'https://api.thecatapi.com/v1/images/search?limit=10');
+
+async function init(url, url_2) {
+    let catFacts = await fetchData(url);
+    let catImages = await fetchImage(url_2);
+    console.log(catFacts[0].text);
+    console.log(catImages); 
+    if (catFacts.length > 0 && catImages && catImages.length > 0) {
+        for (let i = 0; i < 1; i++) {
+            if (catImages[i] && catImages[i].url) {
+                createItem(catFacts[i], catImages[i]);
+            } else {
+                console.log("Bildobjekt fehlt oder hat keine URL.");
+            }
+        }
+    } else {
+        console.log("Leeres Array für Fakten oder Bilder.");
+    }
+}
+
+async function search(searchValue) {
+    let url = `https://cat-fact.herokuapp.com/facts/random?amount=300&text=${searchValue}`;
+    let url_2 = `https://api.thecatapi.com/v1/images/search?limit=10&text=${searchValue}`;
+    popupFact.innerHTML = '';
+    let catFacts = await fetchData(url);
+    let catImages = await fetchImage(url_2);
+    for (let i = 0; i < 1; i++) {
+        createItem(catFacts[i], catImages[i]);
+    }
+}
+
+function createItem(catFact, catImage) {
+    let item = document.createElement('div');
+    item.classList.add('catFact');
+    let imageUrl = catImage && catImage.url ? catImage.url : 'URL nicht verfügbar';
+    item.innerHTML = `
+        <p>${catFact.text}</p>
+        <img src="${imageUrl}" alt="Cat Image">
+        `;
+    popupFact.appendChild(item);
+}
+
+async function fetchData(url) {
+    try {
+        let response = await fetch(url);
+        let data = await response.json();
+        let verifiedFacts = data.filter(fact => fact.status.verified === true);
+        return verifiedFacts;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+async function fetchImage(url_2) {
+    try {
+        let response = await fetch(url_2);
+        let data = await response.json();
+        return data;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
